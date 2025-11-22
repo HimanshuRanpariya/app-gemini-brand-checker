81023f76be86c2a777386df3b2c1e202ba53f6f6
himanshu <20dce099@charusat.edu.in>
2025-11-22 16:16:35 +0530

    fix: add api url

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const API_URL = 'http://localhost:5000/api/check';
+const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/check';
 
 export default function Home() {
   const [prompt, setPrompt] = useState('');
